a6fc87e363cfe99181c8ba66a726d932ae8e4346
Dario Gomez <gomez.dariogs@gmail.com>
2023-06-26 19:35:41 -0300

    added static files

--- taskmgmt/settings.py
+++ taskmgmt/settings.py
@@ -132,10 +132,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 
 # Default primary key field type
